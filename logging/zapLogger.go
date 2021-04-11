2f947fc2a1d471a23a353aeb473f0078d2e7d1b9
keleqnma <cyq65536@gmail.com>
2021-04-11 17:58:42 +0800

    implement multiple proxy modes generate puml images

--- logging/zapLogger.go
+++ logging/zapLogger.go
@@ -1,44 +1,70 @@
 package logging
 
 import (
-	"fmt"
+	"encoding/json"
+
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
-func GetLogger(name string) *zap.SugaredLogger {
+// Global logger
+var Glogger = GetSugar("influx-cluster")
+
+func GetSugar(name string) *zap.SugaredLogger {
 	encoderConfig := zapcore.EncoderConfig{
-		TimeKey:        "time",
-		LevelKey:       "level",
-		NameKey:        "logger",
-		CallerKey:      "caller",
-		MessageKey:     "msg",
-		StacktraceKey:  "stacktrace",
-		LineEnding:     zapcore.DefaultLineEnding,
-		EncodeLevel:    zapcore.LowercaseLevelEncoder, // 小写编码器
-		EncodeTime:     zapcore.ISO8601TimeEncoder,    // ISO8601 UTC 时间格式
-		EncodeDuration: zapcore.SecondsDurationEncoder,
-		EncodeCaller:   zapcore.FullCallerEncoder, // 全路径编码器
+		TimeKey:    "time",
+		LevelKey:   "level",
+		NameKey:    "logger",
+		CallerKey:  "caller",
+		MessageKey: "msg",
+		// StacktraceKey: "stacktrace",
+		LineEnding:  zapcore.DefaultLineEnding,
+		EncodeLevel: zapcore.CapitalLevelEncoder,
+		EncodeTime:  zapcore.ISO8601TimeEncoder,
+		// EncodeDuration: zapcore.SecondsDurationEncoder,
+		EncodeDuration: zapcore.StringDurationEncoder,
+		EncodeCaller:   zapcore.ShortCallerEncoder,
 	}
 
 	// 设置日志级别
 	atom := zap.NewAtomicLevelAt(zap.DebugLevel)
-
 	config := zap.Config{
-		Level:            atom,          // 日志级别
-		Development:      true,          // 开发模式，堆栈跟踪
-		Encoding:         "json",        // 输出格式 console 或 json
-		EncoderConfig:    encoderConfig, // 编码器配置
+		Level:            atom,
+		Development:      false,
+		Encoding:         "console",
+		EncoderConfig:    encoderConfig,
 		InitialFields:    map[string]interface{}{},
 		OutputPaths:      []string{"stdout"},
 		ErrorOutputPaths: []string{"stderr"},
 	}
-
-	// 构建日志
-	loggerRaw, err := config.Build()
-	if err != nil {
-		panic(fmt.Sprintf("log 初始化失败: %v", err))
-	}
+	loggerRaw, _ := config.Build()
 	logger := loggerRaw.Named(name).Sugar()
+	// logger.core.callerSkip += 2
 	return logger
+}
+
+func JsonEncode(data interface{}) string {
+	jsonBytes, _ := json.Marshal(data)
+	return (string(jsonBytes))
+}
+
+// GetLogger get named logger
+func GetLogger(name string) (logger *zap.Logger) {
+	return internalLogger.logger.Named(name)
+}
+
+type zapLogger struct {
+	logger *zap.Logger
+}
+
+var internalLogger *zapLogger
+
+func init() {
+	var logger *zap.Logger
+	var err error
+	logger, err = zap.NewDevelopment()
+	if err != nil {
+		panic(err)
+	}
+	internalLogger = &zapLogger{logger: logger}
 }
